593b44cf6edef505840aece0eb7554432e3cc05f
bjcranda <54648596+bjcranda@users.noreply.github.com>
2019-11-07 19:26:58 -0800

    Added the Demonstation Programs Solution

---
+++ Demonstration Programs/Demonstration Programs/FileManipulation.cs
@@ -0,0 +1,120 @@
+﻿using System.IO;
+using System;
+
+namespace Demonstration_Programs
+
+{   /// <summary>
+    /// Using the classes in the System.IO namespace I'm going to use various methods to create and manipulate files.
+    /// </summary>
+     
+    class Program
+    {
+        static void Main()
+        {
+            tempPath();
+            Console.WriteLine();
+
+            //Creating directory. My example will be C:\temp
+            Console.Write("Specify the folder name and path you want to create: ");
+            string userDirectory = Console.ReadLine();
+            Directory.CreateDirectory(userDirectory);
+            
+            //creating file, examples is mytest.txt
+            Console.Write("Specify the file name you would like to be created in the folder: ");
+            string userFilePath = Console.ReadLine();
+
+            //I combine the directory path with the file name in order to send my function the full location
+            string fullPath = Path.Combine(userDirectory, userFilePath);
+            createFile(fullPath);
+
+            Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("We are going to read whats in the file:");
+            readFile(fullPath);
+
+            Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Would you like to delete a file? (y/n)");
+            string userchoice = Console.ReadLine();
+            userchoice = userchoice.ToLower();
+            if (userchoice == "y")
+            {
+                Console.WriteLine("What is the filepath of the file you want deleted? ");
+                string fileToBeDeleted = Console.ReadLine();
+                deleteFile(fileToBeDeleted);
+            }
+            
+
+            Console.ReadLine();
+
+        }
+
+        //Created a simple function that when called will give us the directory path to the temp folder
+        static void tempPath()
+        {
+            Console.WriteLine("Here is the location of the temp folder.");
+            string tempDirectoryPath = Path.GetTempPath();
+            Console.WriteLine(tempDirectoryPath);
+        }
+
+        //Creating a file on the C drive
+        static void createFile(string userFilePath)
+        {
+            //Checking to see if the file exists. If it does this will not do anything.
+            //If it doesn't exit then we will create the file and add some text.
+            if (!File.Exists(userFilePath))
+            {
+                using (StreamWriter sw = File.CreateText(userFilePath))
+                {
+                    Console.Write("What would you like to put in the file: ");
+                    string userFileText = Console.ReadLine();
+                    sw.WriteLine("This is what you wanted:");
+                    sw.WriteLine();
+                    sw.WriteLine(userFileText);
+                    sw.WriteLine();
+                    sw.WriteLine("C# is cool!");
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(userFilePath))
+                {
+                    Console.Write("What would you like to put in the file: ");
+                    string userFileText = Console.ReadLine();
+                    sw.WriteLine(userFileText);
+                }
+            }
+        }
+
+        //Read from the file just created
+        static void readFile(string userFilePath)
+        {
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(userFilePath))
+            {
+                string read = File.ReadAllText(userFilePath);
+                Console.WriteLine(read);
+            }
+        }
+
+        //Now we will remove the file created
+        static void deleteFile(string userFilePath)
+        {
+
+            // If file found, delete it  
+            if (File.Exists(userFilePath))
+            {
+                File.Delete(userFilePath);
+                Console.WriteLine("File deleted.");
+            }
+            else
+            {
+                Console.WriteLine("file not found");
+            }
+        }
+    }
+
+    
+}
