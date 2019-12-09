49dea9c9856d46945965d156e52ab5984b77d10d
bjcranda <54648596+bjcranda@users.noreply.github.com>
2019-12-09 14:47:24 -0800

    Fixing File Location placement

--- Demonstration Programs/Leetcode/LinkedListCycle.cs
+++ Demonstration Programs/Leetcode/LinkedListCycle.cs
@@ -13,6 +13,22 @@ using System.Text;
 
 namespace Leetcode
 {
+    //class ProgramOutputs
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // LinkedListCycle.........
+
+    //        LinkedListCycle newlist = new LinkedListCycle();
+    //        newlist.AddFront(1);
+    //        newlist.AddFront(2);
+    //        newlist.AddFront(3);
+    //        newlist.AddFront(4);
+    //        newlist.HasCycle();
+    //        newlist.head.next.next.next.next = newlist.head;
+    //        newlist.HasCycle();
+    //    }
+    //}
     public class LinkedListCycle
     {
 
@@ -71,7 +87,7 @@ namespace Leetcode
 
     }
 }
-//MODIFICATIONS: Created this myself
+//MODIFICATIONS: Created this myself using Floyd's algorthm.
 
 //ANALYSIS: Can determine if there is a cycle in a linkedlist
 
--- Demonstration Programs/Leetcode/ProgramOutputs.cs
+++ Demonstration Programs/Leetcode/ProgramOutputs.cs
@@ -2,21 +2,11 @@
 
 namespace Leetcode
 {
-    class ProgramOutputs
-    {
-        static void Main(string[] args)
-        {
-            // LinkedListCycle.........
-
-            LinkedListCycle newlist = new LinkedListCycle();
-            newlist.AddFront(1);
-            newlist.AddFront(2);
-            newlist.AddFront(3);
-            newlist.AddFront(4);
-            newlist.HasCycle();
-            newlist.head.next.next.next.next = newlist.head;
-            newlist.HasCycle();
+    //class ProgramOutputs
+    //{
+    //    static void Main(string[] args)
+    //    {
             
-        }
-    }
+    //    }
+    //}
 }
